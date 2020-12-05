bfbc0b715fee9d8a1c21b698e82850247cef2dc1
Dmitriy Gridchin <tigris.tstu@gmail.com>
2020-12-05 19:57:35 +0300

    осилил поле...

--- src/CrossGame.java
+++ src/CrossGame.java
@@ -6,10 +6,13 @@ public class CrossGame {
     private int dots;
     private final Random rnd;
     private final Scanner sc;
+    private boolean exit;
+    private char[][] field;
 
     public CrossGame() {
         sc = new Scanner(System.in);
         rnd = new Random();
+        exit = false;
     }
 
     public void startGame() {
@@ -18,11 +21,11 @@ public class CrossGame {
 
     public void startGame(int size) {
         if(size < 3) {
-            System.out.printf("Размер поля должен быть не меньше 3!");
+            System.out.println("Размер поля должен быть не меньше 3!");
             size = 3;
         }
         if(size > 20) {
-            System.out.printf("И кто в это будет играть?!");
+            System.out.println("И кто в это будет играть?!");
             size = 20;
         }
         this.size = size;
@@ -35,11 +38,38 @@ public class CrossGame {
         else {
             dots = 5;
         }
+        field = new char[size][size];
 
+        initField();
         mainLoop();
     }
 
-    private void mainLoop() {
+    private void initField() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                field[i][j] = '.';
+            }
+        }
+    }
 
+    private void mainLoop() {
+//        do {
+            printField();
+//        }while (!exit);
+    }
+
+    private void printField() {
+        System.out.print(" *");
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%3d", (i+1));
+        }
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%3d", i+1);
+            for (int j = 0; j < size; j++) {
+                System.out.print(" " + field[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         CrossGame crossGame = new CrossGame();
-        crossGame.startGame();
+        crossGame.startGame(5);
     }
 }
