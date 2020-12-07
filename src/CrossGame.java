c317344ba9d48a2bf3612ee5e2c30456ca01d09d
Dmitriy Gridchin <tigris.tstu@gmail.com>
2020-12-07 11:12:28 +0300

    чуть-чуть доработал

--- src/CrossGame.java
+++ src/CrossGame.java
@@ -8,6 +8,9 @@ public class CrossGame {
     private final Scanner sc;
     private boolean exit;
     private char[][] field;
+    private final char DOT_EMPTY = '•';
+    private final char DOT_X = 'X';
+    private final char DOT_O = 'O';
 
     public CrossGame() {
         sc = new Scanner(System.in);
@@ -20,6 +23,12 @@ public class CrossGame {
     }
 
     public void startGame(int size) {
+        initGame(size);
+        initField();
+        mainLoop();
+    }
+
+    private void initGame(int size) {
         if(size < 3) {
             System.out.println("Размер поля должен быть не меньше 3!");
             size = 3;
@@ -29,33 +38,39 @@ public class CrossGame {
             size = 20;
         }
         this.size = size;
-        if(size >= 3 && size <= 5) {
+        if(size <= 5) {
             dots = 3;
         }
-        else if (size >= 6 && size <= 9) {
+        else if (size <= 9) {
             dots = 4;
         }
         else {
             dots = 5;
         }
         field = new char[size][size];
-
-        initField();
-        mainLoop();
     }
 
     private void initField() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                field[i][j] = '.';
+                field[i][j] = DOT_EMPTY;
             }
         }
     }
 
     private void mainLoop() {
 //        do {
-            printField();
+        printGreetings();
+        printField();
+        turnPlayer();
+        isWin(char ch);
+        turnComputer();
 //        }while (!exit);
+    }
+
+    private void printGreetings() {
+        System.out.println("Крестики-Нолики v.0.0.1");
+        System.out.println("Для хода введите координаты по горизонтали (Х) и вертикали (Y)");
     }
 
     private void printField() {
@@ -71,5 +86,49 @@ public class CrossGame {
             }
             System.out.println();
         }
+    }
+
+    private void turnPlayer() {
+        int x, y;
+        do {
+            x = tryInput("X") - 1;
+            y = tryInput("Y") - 1;
+
+            if(field[y][x] != DOT_EMPTY) {
+                System.out.println("Клетка занята! Попробуйте еще раз");
+                continue;
+            } else {
+                field[y][x] = DOT_X;
+                printField();
+                break;
+            }
+        } while (true);
+    }
+
+    private int tryInput(String s) {
+        int input;
+        do {
+            System.out.print(s + " = ");
+            if (sc.hasNextInt()) {
+                input = sc.nextInt();
+                if (input < 1 || input > size) {
+                    System.out.println("Координата должна быть в преде от 1 до " + size);
+                    continue;
+                }
+            } else {
+                System.out.println("Необходимо ввести число!");
+                sc.next();
+                continue;
+            }
+            return input;
+        } while (true);
+    }
+
+    private boolean isWin(char ch) {
+        return false;
+    }
+
+    private void turnComputer() {
+        // мдя......
     }
 }
